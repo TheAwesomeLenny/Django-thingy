9986021a3a5209a4eeff90cadaa4f8bd5a6ea6ef
Yesh Loony <TheAwesomeLenny@gmail.com>
2020-04-12 21:00:40 +0000

    Final adjustments to HTML and CSS

--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
     url(r'^$', views.course_list, name='list'),
     url(r'(?P<course_pk>\d+)/(?P<step_pk>\d+)/$', views.step_detail, name='step'),
     url(r'(?P<pk>\d+)/$', views.course_detail, name='detail'),
-    
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
